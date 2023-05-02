92f212faffddcdc3c1b9eace77b303e0880e7a39
Siddharth Bhatia <sidb60200@gmail.com>
2023-05-01 20:23:05 -0500

    add default keyword arguments to run_scene_graph method

--- examples/run_game.jl
+++ examples/run_game.jl
@@ -4,4 +4,4 @@ const story_file_name = ARGS[1]
 
 const scene_graph = STA.create_scene_graph(story_file_name)
 
-STA.run_scene_graph(scene_graph, "start_scene", "end_scene", 0.01)
+STA.run_scene_graph(scene_graph)
