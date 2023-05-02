ef7b0b845bb14cad324ceddde1302320520b3141
Siddharth Bhatia <sidb60200@gmail.com>
2023-05-01 19:46:11 -0500

    animate scene text that can be interrupted by pressing a key

--- src/SimpleTextAdventure.jl
+++ src/SimpleTextAdventure.jl
@@ -1,5 +1,6 @@
 module SimpleTextAdventure
 
+import REPL
 import REPL.TerminalMenus as TM
 import YAML
 
@@ -25,16 +26,35 @@ end
 function run_scene_graph(scene_graph, start_scene_name, end_scene_name)
     scene_name = start_scene_name
 
+    terminal = TM.terminal
+    terminal_out = terminal.out_stream
+    terminal_in = terminal.in_stream
+
     while scene_name != end_scene_name
         scene = scene_graph[scene_name]
 
         scene_text = scene["text"]
+        print(terminal_out, CLEAR_SCREEN)
+        print(terminal_out, MOVE_CURSOR_TO_ORIGIN)
 
-        print(CLEAR_SCREEN)
-        print(MOVE_CURSOR_TO_ORIGIN)
-        println(scene_text)
-        println()
-        println()
+        REPL.Terminals.raw!(terminal, true)
+        Base.start_reading(terminal_in)
+
+        for char in scene_text
+            print(terminal_out, char)
+            if iszero(bytesavailable(terminal_in))
+                sleep(0.01)
+            end
+        end
+
+        print(terminal_out, '\n')
+        print(terminal_out, '\n')
+        print(terminal_out, '\n')
+
+        read(terminal_in, bytesavailable(terminal_in))
+
+        Base.stop_reading(terminal_in)
+        REPL.Terminals.raw!(terminal, false)
 
         choices = scene["choices"]
         choice = TM.request(TM.RadioMenu([choice["text"] for choice in choices]))
