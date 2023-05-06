10e6315c14cc2aa083e44d799692fa16968e0a30
Siddharth Bhatia <sidb60200@gmail.com>
2023-05-06 16:33:35 -0500

    create package SimpleTextAdventureApp

---
+++ compile/SimpleTextAdventureApp/src/SimpleTextAdventureApp.jl
@@ -0,0 +1,15 @@
+module SimpleTextAdventureApp
+
+import SimpleTextAdventure as STA
+
+function julia_main()::Cint
+    story_file_name = ARGS[1]
+
+    scene_graph = STA.create_scene_graph(story_file_name)
+
+    STA.run_scene_graph(scene_graph)
+
+    return 0
+end
+
+end # module SimpleTextAdventureApp
